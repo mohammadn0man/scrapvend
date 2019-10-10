178377d13fc15053f6be1015bf1328331f15000c
Mohammad Noman <mohammad.noman@live.in>
2019-10-10 20:01:38 +0530

    few changes to contactframent.java and additem.java

--- android/scrapvend/app/src/main/java/com/example/scrapvend/ui/contact/ContactFragment.java
+++ android/scrapvend/app/src/main/java/com/example/scrapvend/ui/contact/ContactFragment.java
@@ -76,7 +76,7 @@ public class ContactFragment extends Fragment {
 
     private class MyTask extends AsyncTask<Void, Void, Void> {
 
-        private String text1="", text2="", text3="", text4="";
+        private String text1 = "", text2 = "", text3 = "", text4 = "";
 
         @Override
         protected Void doInBackground(Void... voids) {
@@ -120,8 +120,7 @@ public class ContactFragment extends Fragment {
         @Override
         protected void onPostExecute(Void aVoid) {
 
-            Log.d(TAG,  "onPostExecute");
-
+            Log.d(TAG, "onPostExecute");
             textView1.setText(text1);
             textView2.setText(text2);
             textView3.setText(text3);
@@ -130,65 +129,4 @@ public class ContactFragment extends Fragment {
             super.onPostExecute(aVoid);
         }
     }
-
-//    public static Connection getMySqlConnection()    {
-//        /* Declare and initialize a sql Connection variable. */
-//        java.sql.Connection ret = null;
-//
-//        try
-//        {
-//
-//            Log.d(TAG, "before com.mysql.jdbc.Driver");
-//            /* Register for jdbc driver class. */
-//            Class.forName("com.mysql.jdbc.Driver");
-//
-//            Log.d(TAG, "after com.....");
-//
-//            /* Create connection url. */
-//            String mysqlConnUrl = "jdbc:mysql://10.0.2.2:3306/mydb";
-//
-//            /* db user name. */
-//            String mysqlUserName = "root";
-//
-//            /* db password. */
-//            String mysqlPassword = "admin";
-//
-//            /* Get the Connection object. */
-//            ret = DriverManager.getConnection(mysqlConnUrl, mysqlUserName , mysqlPassword);
-//
-//            Log.d(TAG, "after getConnection");
-//
-//            /* Get related meta data for this mysql server to verify db connect successfully.. */
-//            DatabaseMetaData dbmd = ret.getMetaData();
-//
-//            String dbName = dbmd.getDatabaseProductName();
-//
-//            String dbVersion = dbmd.getDatabaseProductVersion();
-//
-//            String dbUrl = dbmd.getURL();
-//
-//            String userName = dbmd.getUserName();
-//
-//            String driverName = dbmd.getDriverName();
-//
-//            Log.d(TAG, "Database Name is " + dbName);
-//
-//            Log.d(TAG, "Database Version is " + dbVersion);
-//
-//            Log.d(TAG, "Database Connection Url is " + dbUrl);
-//
-//            Log.d(TAG, "Database User Name is " + userName);
-//
-//            Log.d(TAG, "Database Driver Name is " + driverName);
-//
-//        }catch(Exception ex)
-//        {
-//            ex.printStackTrace();
-//        }finally
-//        {
-//            Log.d(TAG, "inside Finally block");
-//            return ret;
-//        }
-//    }
-
 }
--- android/scrapvend/app/src/main/java/com/example/scrapvend/ui/pricing/AddItem.java
+++ android/scrapvend/app/src/main/java/com/example/scrapvend/ui/pricing/AddItem.java
@@ -1,36 +1,50 @@
 package com.example.scrapvend.ui.pricing;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.scrapvend.DatabaseConnect.MySqlConnector;
+import com.example.scrapvend.Models.ItemModel;
 import com.example.scrapvend.R;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AddItem extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    ImageView imageView;
+    private final String TAG = "MyActivity";
+    ImageView itemImageView;
     Button chooseImage;
     Button addItem;
     Uri imageUri;
     private static final int PICK_IMAGE = 100;
+    private TextView itemNameView, itemRateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        imageView = (ImageView) findViewById(R.id.item_image);
+        itemImageView = (ImageView) findViewById(R.id.item_image);
         chooseImage = (Button) findViewById(R.id.choose_item_image);
 
         chooseImage.setOnClickListener(new View.OnClickListener() {
@@ -40,14 +54,67 @@ public class AddItem extends AppCompatActivity implements AdapterView.OnItemSele
             }
         });
 
-        Spinner spinner = findViewById(R.id.spinner_measure);
+        Spinner itemSpinner = findViewById(R.id.spinner_measure);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.item_measure, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        itemSpinner.setAdapter(adapter);
+        itemSpinner.setOnItemSelectedListener(this);
 
+        itemNameView = (TextView) findViewById(R.id.item_name);
+        itemRateView = (TextView) findViewById(R.id.item_rate);
 
+        addItem = (Button) findViewById(R.id.add_item_submit);
 
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new InsertIntoDatabaseTask().execute();
+            }
+        });
+
+    }
+
+    private class InsertIntoDatabaseTask extends AsyncTask<Void, Void, Void> {
+
+        ItemModel itemModel = new ItemModel();
+        String name = "", image = "", rate = "", measure = "";
+        Bitmap bitmapImage;
+        byte[] byteImage;
+
+//        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            Log.d(TAG, "inside doInBackground");
+
+            try {
+                MySqlConnector connection = new MySqlConnector();
+
+                Connection conn = connection.getMySqlConnection();
+
+                Log.d(TAG, "Connection established");
+
+                Statement statement = conn.createStatement();
+
+                Log.d(TAG, "Statement created");
+
+                itemModel.setItemName(itemNameView.getText().toString());
+                name = itemNameView.getText().toString();
+                rate = itemRateView.getText().toString();
+                float floatRate = Float.parseFloat(rate);
+                bitmapImage = ((BitmapDrawable)itemImageView.getDrawable()).getBitmap();
+
+                statement.executeUpdate("INSERT INTO `item_details`(`Item_name`, `Item_rate`, `Item_measure') VALUES (name, floatRate, ,'Kilo-grams');");
+
+                Log.d(TAG, "query executed");
+
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 
     private void openGallery() {
@@ -60,9 +127,9 @@ public class AddItem extends AppCompatActivity implements AdapterView.OnItemSele
         super.onActivityResult(request, resultCode, data);
         if(resultCode == RESULT_OK && request == PICK_IMAGE){
             imageUri = data.getData();
-            imageView.setImageURI(imageUri);
+            itemImageView.setImageURI(imageUri);
         }
-    }
+    }  // ###########
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
