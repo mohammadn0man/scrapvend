f4006e262c76852d97d3497ec2564760f01007c1
bob-pro <saifali97.sa7@gmail.com>
2019-10-29 02:59:04 +0530

    registration done without otp

---
+++ android/scrapvend/login/src/main/java/com/example/login/registration_user.java
@@ -0,0 +1,115 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class registration_user extends AppCompatActivity {
+    private Button register;
+    public EditText _name, _email, _password, _re_password, _number;
+    public String name, email, password, re_password, number;
+    MySqlConnector mySqlConnector;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.registration_user);
+        getSupportActionBar().hide();
+
+        _name = (EditText) findViewById(R.id.name);
+        _email = (EditText) findViewById(R.id.email);
+        _number = (EditText) findViewById(R.id.number);
+        _password = (EditText) findViewById(R.id.password);
+        _re_password = (EditText) findViewById(R.id.repassword);
+        register = (Button) findViewById(R.id.register);
+
+        mySqlConnector = new MySqlConnector();
+
+        progressBar = new ProgressBar(this);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = _name.getText().toString();
+                email = _email.getText().toString();
+                number = _number.getText().toString();
+                password = _password.getText().toString();
+                re_password = _re_password.getText().toString();
+                if (name.trim().equals("") || email.trim().equals("") || number.trim().equals("") || password.trim().equals("")) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Please enter all the fields",
+                            Toast.LENGTH_SHORT);
+
+                    toast.show();
+                } else if (password.compareTo(re_password) != 0) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Password should be same in both the fields",
+                            Toast.LENGTH_SHORT);
+
+                    toast.show();
+                } else {
+                    new Openlogin().execute();
+                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+    }
+
+    public class Openlogin extends AsyncTask<String, String, String> {
+        boolean isSuccess = false;
+
+        @Override
+        public String doInBackground(String... params) {
+            try {
+                Connection con = mySqlConnector.getMySqlConnection();
+                if (con == null) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Please check your INTERNET connection",
+                            Toast.LENGTH_SHORT);
+
+                    toast.show();
+                } else {
+                    String query1 = "INSERT INTO `login_info`(`Username`, `Role`, `password`, `email`, `contact_no`) VALUES (\"" + name + number + "\", '2',\"" + password + "\",\"" + email + "\",\"" + number + "\")";
+                    String query2 = "INSERT INTO `user_details`(`Name`, `Username`) VALUES (\"" + name + "\",\"" + name + number + "\")";
+                    Log.d("Reg", "query = " + query1);
+                    Statement statement = con.createStatement();
+                    statement.executeUpdate(query1);
+                    statement.executeUpdate(query2);
+
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Register successful",
+                            Toast.LENGTH_SHORT);
+                    toast.show();
+
+                    isSuccess = true;
+
+                }
+
+            } catch (SQLException e) {
+                isSuccess = false;
+            }
+
+            return null;
+    }
+
+
+}
+
+
+
+}
