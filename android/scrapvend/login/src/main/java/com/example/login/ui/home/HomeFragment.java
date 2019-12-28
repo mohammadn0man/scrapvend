42cc39c786ed973595e1a4f5eb7d90e8918b4140
Poornima <poornimavasrhney718@gmail.com>
2019-12-29 01:08:06 +0530

    image slider added

--- android/scrapvend/login/src/main/java/com/example/login/ui/home/HomeFragment.java
+++ android/scrapvend/login/src/main/java/com/example/login/ui/home/HomeFragment.java
@@ -11,25 +11,27 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.login.MainActivity;
 import com.example.login.R;
+import com.example.login.ui.Adapter.HomePageImageAdapter;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-
+    ViewPager viewPager;
+    int images[] = {R.drawable.image1, R.drawable.image_2, R.drawable.image_3, R.drawable.image_4};
+     HomePageImageAdapter homePageImageAdapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        viewPager = (ViewPager) root.findViewById(R.id.viewPager);
+
+        homePageImageAdapter = new HomePageImageAdapter(getActivity(), images);
+        viewPager.setAdapter(homePageImageAdapter);
         return root;
     }
 }
