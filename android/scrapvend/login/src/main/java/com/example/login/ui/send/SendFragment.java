2d44a29f15d2e15ba534245a6aa77c2b1e3ebf15
Poornima <poornimavasrhney718@gmail.com>
2019-12-31 03:40:14 +0530

    session created in pickupPerson

--- android/scrapvend/login/src/main/java/com/example/login/ui/send/SendFragment.java
+++ android/scrapvend/login/src/main/java/com/example/login/ui/send/SendFragment.java
@@ -1,5 +1,6 @@
 package com.example.login.ui.send;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.login.R;
+import com.example.login.logout;
 
 public class SendFragment extends Fragment {
 
@@ -20,16 +22,13 @@ public class SendFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        sendViewModel =
-                ViewModelProviders.of(this).get(SendViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_send, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
-        sendViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+//        sendViewModel =
+//                ViewModelProviders.of(this).get(SendViewModel.class);
+//        View root = inflater.inflate(R.layout.fragment_send, container, false);
+        //final TextView textView = root.findViewById(R.id.text_send);
+        Intent intent = new Intent(getActivity(), logout.class);
+        startActivity(intent);
+
+        return null;
     }
 }
