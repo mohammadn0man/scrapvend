88d87a840bdf3e881699744c29549465127044cb
Poornima <poornimavasrhney718@gmail.com>
2020-01-05 03:48:09 +0530

    updated

--- android/scrapvend/login/src/main/java/com/example/login/userUi/home/HomeFragment.java
+++ android/scrapvend/login/src/main/java/com/example/login/userUi/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,6 +24,7 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     TextView textRequest,textList,textBooking;
+
     ViewPager viewPager;
     Timer timer;
     int images[] = {R.drawable.image1, R.drawable.image_2, R.drawable.image_3, R.drawable.image_4};
