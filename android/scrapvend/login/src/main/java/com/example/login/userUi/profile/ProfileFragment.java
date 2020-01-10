763eafce745a2a8d0247301a57015d9f0efb13f0
Poornima <poornimavasrhney718@gmail.com>
2020-01-11 03:22:25 +0530

    merge conflict resolve

--- android/scrapvend/login/src/main/java/com/example/login/userUi/profile/ProfileFragment.java
+++ android/scrapvend/login/src/main/java/com/example/login/userUi/profile/ProfileFragment.java
@@ -1,7 +1,6 @@
 package com.example.login.userUi.profile;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
