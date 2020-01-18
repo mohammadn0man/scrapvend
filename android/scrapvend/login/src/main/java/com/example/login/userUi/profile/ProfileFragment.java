ab74bccb3d5ed8569d966bf1fefdef3aa6344652
Mohammad Noman <mohammad.noman@live.in>
2020-01-18 07:27:51 +0530

    pickup person updated

--- android/scrapvend/login/src/main/java/com/example/login/userUi/profile/ProfileFragment.java
+++ android/scrapvend/login/src/main/java/com/example/login/userUi/profile/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
